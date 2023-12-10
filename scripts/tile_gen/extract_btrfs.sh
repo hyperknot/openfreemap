1def82c205483890d1d7533d1ef1676136f77733
Zsolt Ero <zsolt.ero@gmail.com>
2023-12-10 06:46:11 +0100

    fix

--- scripts/tile_gen/extract_btrfs.sh
+++ scripts/tile_gen/extract_btrfs.sh
@@ -5,14 +5,8 @@ rm -rf mnt
 rm -f image.btrfs
 
 
-# make a sparse file
-# make sure it's bigger then the current OSM output
+# make an empty file that's definitely bigger then the current OSM output
 fallocate -l 300G image.btrfs
-
-fallocate -l 10G image.btrfs
-
-
-
 
 
 # metadata: single needed as default is now DUP
@@ -22,12 +16,11 @@ mkfs.btrfs -v \
 
 # https://btrfs.readthedocs.io/en/latest/btrfs-man5.html#mount-options
 # compression doesn't make sense, data is already gzip compressed
-
 mkdir -p mnt
 sudo mount -v \
   -t btrfs \
   -o noacl,nobarrier,noatime,max_inline=4096 \
-  image2.btrfs mnt
+  image.btrfs mnt
 
 
 sudo chown ofm:ofm -R mnt
@@ -35,15 +28,6 @@ sudo chown ofm:ofm -R mnt
 ../../tile_gen/venv/bin/python ../../tile_gen/extract.py output.mbtiles mnt/extract \
   > "extract_out.log" 2> "extract_err.log"
 
-
-# resize to min possible size
-btrfs filesystem usage -b mnt
-
-btrfs filesystem resize -10G mnt
-sudo btrfs filesystem resize -100M mnt
-
-
-sudo umount mnt
 
 
 
