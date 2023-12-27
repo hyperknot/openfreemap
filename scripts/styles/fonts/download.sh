130cc7f4d157258d75b98dfd5e518fb9bb5ce1c8
Zsolt Ero <zsolt.ero@gmail.com>
2023-12-27 21:56:52 +0100

    fonts

---
+++ scripts/styles/fonts/download.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/maplibre/demotiles.git maplibre-demotiles --depth=1
+git clone https://github.com/openmaptiles/fonts.git openmaptiles-fonts --depth=1
+git clone https://github.com/protomaps/basemaps-assets.git protomaps-assets --depth=1
+
+
+
+
+# wget https://codeload.github.com/maplibre/demotiles/zip/refs/heads/gh-pages -O maplibre-fonts.zip
+
+# rm -rf demotiles-gh-pages
+# unzip maplibre-fonts.zip
+# rm maplibre-fonts.zip
+
