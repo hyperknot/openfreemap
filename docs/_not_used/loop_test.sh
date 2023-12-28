4cde507741a6d347943117815a54bd12e0f4ac8e
Zsolt Ero <zsolt.ero@gmail.com>
2023-12-28 20:22:12 +0100

    loop test

---
+++ docs/_not_used/loop_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Define the source folder
+source_folder="20231228_201550_pt"
+
+# Define the number of copies you want to make
+number_of_copies=30
+
+# Loop and copy the folder into c1, c2, c3, c4, ...
+for i in $(seq 1 $number_of_copies); do
+  cp -r "$source_folder" "c$i"
+done
