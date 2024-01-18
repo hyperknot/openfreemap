e32079a97f54dd20ee4b091dfd915534139ce422
Zsolt Ero <zsolt.ero@gmail.com>
2024-01-18 01:46:44 +0100

    map_howto

--- website/assets/support_plans.js
+++ website/assets/support_plans.js
@@ -1,22 +1,22 @@
-const slider = document.getElementById('support-plans-slider');
+const slider = document.getElementById('support-plans-slider')
 
-var arbitraryValuesForSlider = ['128MB', '256MB', '1GB', '8GB', '16GB', '32GB'];
+var arbitraryValuesForSlider = ['128MB', '256MB', '1GB', '8GB', '16GB', '32GB']
 
 var format = {
-    to: function(value) {
-        return arbitraryValuesForSlider[Math.round(value)];
-    },
-    from: function (value) {
-        return arbitraryValuesForSlider.indexOf(value);
-    }
-};
+  to: function (value) {
+    return arbitraryValuesForSlider[Math.round(value)]
+  },
+  from: function (value) {
+    return arbitraryValuesForSlider.indexOf(value)
+  },
+}
 
 noUiSlider.create(slider, {
-    // start values are parsed by 'format'
-    start: '1GB',
-    range: { min: 0, max: arbitraryValuesForSlider.length - 1 },
-    step: 1,
-    tooltips: true,
-    format: format,
-    pips: { mode: 'steps', format: format, density: 50 },
-});
+  // start values are parsed by 'format'
+  start: '1GB',
+  range: { min: 0, max: arbitraryValuesForSlider.length - 1 },
+  step: 1,
+  tooltips: true,
+  format: format,
+  pips: { mode: 'steps', format: format, density: 50 },
+})
