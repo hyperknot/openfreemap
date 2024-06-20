8b3bd33766481245c589b72eea5375eda704fdc8
Zsolt Ero <zsolt.ero@gmail.com>
2024-06-20 10:42:51 +0200

    nested css

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+
+export default defineConfig({
+  vite: {
+    css: {
+      transformer: "lightningcss",
+    },
+  },
+})
+
+
