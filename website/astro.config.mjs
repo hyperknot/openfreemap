a63dfb348ccbbe60a3b7d27563c72290a9f7fb53
Zsolt Ero <zsolt.ero@gmail.com>
2025-02-27 16:07:43 +0100

    sitemap

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 
 export default defineConfig({
+  site: 'https://openfreemap.org',
   vite: {
     css: {
       transformer: 'lightningcss',
     },
   },
+  integrations: [sitemap()],
 })
