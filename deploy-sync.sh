7e8dcbaa4a04bd31a4f029e24e8a91fe5f451762
Zsolt Ero <zsolt.ero@gmail.com>
2025-02-27 04:40:57 +0100

    prepare-virtualenv, cleaning

--- deploy-sync.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export ENV=prod
-
-./init-server.py http-host-sync ofm-h-fi-1 -y
-./init-server.py http-host-sync ofm-h-de-2 -y
-
--- prepare-virtualenv.sh
+++ prepare-virtualenv.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-find . -name "*.egg-info" -exec rm -rf {} +
-find . -name __pycache__ -exec rm -rf {} +
-
-# deactivate
-rm -rf venv
-python3 -m venv venv
-source venv/bin/activate
-
-pip install -U pip wheel setuptools
-
-pip install -e .
-pip install -e modules/http_host
-pip install -e modules/tile_gen
-pip install -e modules/loadbalancer
+find . -name '*.egg-info' -type d -prune -exec rm -rf {} +
+find . -name '*.pyc' -delete
+find . -name __pycache__ -type d -prune -exec rm -rf {} +
+find . -name .DS_Store -delete
+find . -name .ipynb_checkpoints -exec rm -rf {} +
+find . -name .pytest_cache -exec rm -rf {} +
+find . -name .ruff_cache -exec rm -rf {} +
+find . -name .venv -type d -prune -exec rm -rf {} +
+find . -name venv -type d -prune -exec rm -rf {} +
 
 
+uv venv --python=3.12
+source .venv/bin/activate
 
+
+uv pip install -e .
+uv pip install -e modules/http_host
+uv pip install -e modules/tile_gen
+uv pip install -e modules/loadbalancer
