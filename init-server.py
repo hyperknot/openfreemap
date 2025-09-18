ab86f9a5fe6869d6f49eca39facf0c086cd4220b
Zsolt Ero <zsolt.ero@gmail.com>
2025-09-18 18:45:23 +0200

    removed loadbalancer and unused parts

--- init-server.py
+++ init-server.py
@@ -9,8 +9,6 @@ from ssh_lib.tasks import (
     prepare_shared,
     prepare_tile_gen,
     run_http_host_sync,
-    setup_loadbalancer,
-    setup_roundrobin_writer,
 )
 from ssh_lib.utils import (
     put,
@@ -111,29 +109,6 @@ def tile_gen(
 
     prepare_shared(c)
     prepare_tile_gen(c, enable_cron=cron)
-
-
-@cli.command()
-@common_options
-def roundrobin_dns_writer(hostname, user, port, noninteractive):
-    if not noninteractive and not click.confirm(f'Run script on {hostname}?'):
-        return
-
-    c = get_connection(hostname, user, port)
-
-    setup_roundrobin_writer(c)
-
-
-@cli.command()
-@common_options
-def loadbalancer(hostname, user, port, noninteractive):
-    if not noninteractive and not click.confirm(f'Run script on {hostname}?'):
-        return
-
-    c = get_connection(hostname, user, port)
-    prepare_shared(c)
-
-    setup_loadbalancer(c)
 
 
 @cli.command()
