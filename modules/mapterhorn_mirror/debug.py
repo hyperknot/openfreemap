8c3e6eaff9413892751866d1c8591f8fd20069f1
Zsolt Ero <zsolt.ero@gmail.com>
2025-10-23 11:43:56 +0200

    work

---
+++ modules/mapterhorn_mirror/debug.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#
+# ]
+# ///
+import subprocess
+from pathlib import Path
+from urllib.parse import urlparse
+
+
+rclone_config = Path('../../config/rclone.conf')
+
+url = 'https://download.mapterhorn.com/planet.pmtiles'
+
+# Parse URL properly
+parsed = urlparse(url)
+base_url = f'{parsed.scheme}://{parsed.netloc}'
+path = parsed.path.lstrip('/')
+
+destination = '.'
+
+subprocess.run(
+    [
+        'rclone',
+        'ls',
+        '--http-url',
+        base_url,
+        f':http:{path}',
+        # destination,
+        '--dump',
+        'headers',
+        '-vv',
+        '--config',
+        rclone_config,
+    ]
+)
