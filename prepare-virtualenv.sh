c508d41a43acbc0a39ab93b3571b663c22dd9cdc
Zsolt Ero <zsolt.ero@gmail.com>
2026-05-25 16:29:35 +0200

    work

--- prepare-virtualenv.sh
+++ prepare-virtualenv.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 find . -name '*.egg-info' -type d -prune -exec rm -rf {} +
 find . -name '*.pyc' -delete
-find . -name __pycache__ -type d -prune -exec rm -rf {} +
 find . -name .DS_Store -delete
 find . -name .ipynb_checkpoints -exec rm -rf {} +
+find . -name .mypy_cache -type d -prune -exec rm -rf {} +
+find . -name .pnpm-store -exec rm -rf {} +
 find . -name .pytest_cache -exec rm -rf {} +
 find . -name .ruff_cache -exec rm -rf {} +
+find . -name .venv -type d -prune -exec rm -rf {} +
+find . -name __pycache__ -type d -prune -exec rm -rf {} +
+find . -name node_modules -type d -prune -exec rm -rf {} +
 find . -name venv -type d -prune -exec rm -rf {} +
 
+
+
+# uv
 uv sync
+
+pnpm i --verbose
