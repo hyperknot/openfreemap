669ebaaca915c2faebd369c55267af35cd856b78
Zsolt Ero <zsolt.ero@gmail.com>
2026-05-26 12:57:22 +0200

    work

--- lint.sh
+++ lint.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+find . -type d -name __pycache__ -prune -exec rm -rf {} +
+
 if ! command -v hk >/dev/null 2>&1; then
   brew install hk
 fi
 hk install
-
 
 hk fix --all
 
