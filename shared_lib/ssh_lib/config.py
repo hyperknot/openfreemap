47ecbcb57b9c0cc7256f0fba2d76c84cd8a0eacd
Zsolt Ero <zsolt.ero@gmail.com>
2026-05-27 22:16:08 +0200

    work

--- shared_lib/ssh_lib/config.py
+++ shared_lib/ssh_lib/config.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+from typing import ClassVar
 
 
 class Configuration:
     # local paths relative to this file
-    local_assets_dir = Path(__file__).parent.parent / 'assets'
+    local_assets_dir: ClassVar[Path] = Path(__file__).parent.parent / 'assets'
 
 
 config = Configuration()
